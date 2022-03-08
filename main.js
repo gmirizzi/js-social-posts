37402428d2147acb7ba5cd83b04fcbb2e05167ce
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-03-08 14:01:49 +0100

    creata la function per stampare i post in html

--- main.js
+++ main.js
@@ -55,3 +55,38 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+function addPost(content, media, authorImg, name, likes, date, id) {
+    const post = document.createElement("div");
+    post.classList.add("post");
+    post.innerHTML =
+        `<div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                <img class="profile-pic" src="${authorImg}" alt="${name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${name}</div>
+                    <div class="post-meta__time">${date}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${content}</div>
+        <div class="post__image">
+            <img src="${media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>`;
+    document.getElementById("container").append(post);
+}
